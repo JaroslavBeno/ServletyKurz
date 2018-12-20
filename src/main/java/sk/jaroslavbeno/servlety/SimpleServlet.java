caf98cc1de9c4d429577fe62f9306c6238f83097
beno <jaroslav.beno@posam.sk>
2018-12-20 06:32:06 +0100

    11 Spracúvam parametre z url adresy

--- src/main/java/sk/jaroslavbeno/servlety/SimpleServlet.java
+++ src/main/java/sk/jaroslavbeno/servlety/SimpleServlet.java
@@ -13,6 +13,20 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Som v SimpleServlet</h1>");
+        out.println("<!DOCTYPE html> <html><body>");
+
+        out.print("<h1>Som v SimpleServlet</h1>");
+
+        String meno = req.getParameter("meno");
+        String priezvisko = req.getParameter("priezvisko");
+
+        out.println("<br>");
+        out.printf("meno: %s", meno);
+        out.println("<br>");
+        out.printf("priezvisko: %s", priezvisko);
+
+        out.println("</body> </html>");
+
+
     }
 }
