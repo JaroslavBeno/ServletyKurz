5f992b6f95c0abf3236665ce67d3350a1b2f97c4
beno <jaroslav.beno@posam.sk>
2018-12-20 06:41:33 +0100

    12 Posielame dáta cez formulár a spracúvame ich v doPost

--- src/main/java/sk/jaroslavbeno/servlety/SimpleServlet.java
+++ src/main/java/sk/jaroslavbeno/servlety/SimpleServlet.java
@@ -29,4 +29,24 @@ public class SimpleServlet extends HttpServlet {
 
 
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String meno = req.getParameter("meno");
+        String stranka = req.getParameter("stranka");
+
+//        resp.sendRedirect(String.format("simpleservlet?meno=%s&stranka=%s", meno, stranka));
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html> <html><body>");
+
+        out.print("<h1>Som v SimpleServlet POST</h1>");;
+
+        out.println("<br>");
+        out.printf("meno: %s", meno);
+        out.println("<br>");
+        out.printf("stranka: %s", stranka);
+
+        out.println("</body> </html>");
+
+    }
 }
