f5bc833d22e68782cb4aa2f089f36367bcbbffdc
beno <jaroslav.beno@posam.sk>
2018-12-20 07:23:51 +0100

    19 Vytvárame prvý filter

---
+++ src/main/java/sk/jaroslavbeno/filtre/FirstFilter.java
@@ -0,0 +1,26 @@
+package sk.jaroslavbeno.filtre;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/*"},
+        filterName = "FirstFilter name",
+        initParams = {@WebInitParam(name = "meno", value = "Jarko Beno")})
+public class FirstFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
