82f66c7b43061386341935b7c9cd2a969f5e492d
beno <jaroslav.beno@posam.sk>
2018-12-20 06:08:58 +0100

    07 Čo je obhsaom url adresy - nastavujeme WebServlet anotáciu

--- src/main/java/sk/jaroslavbeno/servlety/SimpleServlet.java
+++ src/main/java/sk/jaroslavbeno/servlety/SimpleServlet.java
@@ -1,7 +1,18 @@
 package sk.jaroslavbeno.servlety;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = {"/simpleservlet", "/daco"})
 public class SimpleServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Som v SimpleServlet</h1>");
+    }
 }
